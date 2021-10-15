8dec53febef7465fda88ef8cc926adfe3a4dc3b9
Gaelfergar <gfernandezg1900@alumno.ipn.mx>
2021-10-15 00:59:15 -0500

    Avance AES

--- 05_AES_Web/js/aes.js
+++ 05_AES_Web/js/aes.js
@@ -1,13 +1,21 @@
-var mensaje = "había una vez un pato malvado...";
+let password = "";
 
-//la llave puede ser de menos o más de 16 caracteres gracias al padding
-var password = "qwertyui";
-
-var cifrado = CryptoJS.AES.encrypt(mensaje, password);
-
-var descifrado = CryptoJS.AES.decrypt(cifrado, password);
-
-document.getElementById("demo00").innerHTML = mensaje;
-document.getElementById("demo01").innerHTML = cifrado;
-document.getElementById("demo02").innerHTML = descifrado;
-document.getElementById("demo03").innerHTML = descifrado.toString(CryptoJS.enc.Utf8);
+$(document).ready(function(){
+    $('#ci').click(function(){
+        //el mensaje
+        let mensaje = document.getElementById("demo00").value;
+        password = document.getElementById('llave').value;
+        var cifrado = CryptoJS.AES.encrypt(mensaje, password);
+        //el cifrado
+        document.getElementById("demo01").value = cifrado;
+        var descifrado = CryptoJS.AES.decrypt(cifrado, password);
+        //el descifrado sin formato
+        document.getElementById("demo02").value = descifrado;
+        //el descifrado con formato
+        document.getElementById("demo03").innerHTML = descifrado.toString(CryptoJS.enc.Utf8);
+    });
+    $('#de').click(function(){
+        
+        
+    });
+});
